222162094b123e0b66dc9f17e72931981a69d73f
revital-m <revital13m@gmail.com>
2022-01-09 12:56:22 +0200

    starting to learn NODEJS

---
+++ 1_1_node_vs_js/1_1_node_vs_js.js
@@ -0,0 +1,24 @@
+/** 
+//? 1.
+//* file system access.
+    reason: nodejs uses v8 engine that written in c++ which support file system access, so the nodejs also supports that function.
+
+//* global.
+    reason: the global gives the functionality needed same as we have in the window for the browser.
+
+//* readline.
+    reason: an interface for reading data from a Readable stream one line at a time.
+*/
+
+/** 
+//? 2.
+//* window.
+    reason: the nodejs doesnt work with the DOM and the window object works with the DOM.
+
+//* query selectors.
+    reason: the nodejs doesnt work with the DOM and the window object works with the DOM.
+
+
+//* Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox. 
+    reason: Javascript is a programming language and the nodejs is a runtime environment.
+*/
