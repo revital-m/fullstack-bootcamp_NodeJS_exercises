98f29291bb60ac0c953eb072880755c75c555d2c
revital-m <revital13m@gmail.com>
2022-01-10 08:59:33 +0200

    ex 2.1

---
+++ 2_1_file_system/2_1_file_system.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+// fs.writeFileSync('newTextFile.txt', 'Q-1: My new text file.');
+
+// fs.copyFileSync('./newTextFile.txt', 'myCopyFile.txt');
+
+// fs.renameSync('newTextFile.txt', 'changeName.txt');
+
