ade9602ffa8a74033c0bb2024b91fbbfdb11b163
revital-m <revital13m@gmail.com>
2022-01-10 08:59:44 +0200

    ex 3.1

---
+++ 3_1_import_export/3_1_import_export.js
@@ -0,0 +1,16 @@
+/**
+//? 1. What is the difference between import and require?
+Import: Is used to refer to an ES module. Other file types can’t be imported. They are permitted only in ES modules and the specifier of this statement can either be a URL-style relative path or a package name.
+
+Require: The basic functionality of require is that it reads a JavaScript file, executes the file, and then proceeds to return the export object. It not only allows you to add built-in core Node modules but also community-based modules (node_modules), and local modules in the desired program. There are various inbuilt modules in a node like – HTTP module, URL module, query string module, path module and more.
+
+//? 2. How can you enable using the import syntax using node js?
+If you want to use import module then you have to save the file with '.mjs' extension.
+
+//? 3. Give 2 node.js environment variables that are not available when using the import syntax.
+
+//? 4. Create 3 functions using the export/import syntax.
+
+//? 5. Import the file system module using the import syntax.
+
+*/
