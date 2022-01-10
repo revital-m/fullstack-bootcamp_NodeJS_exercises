70db65b9968518437ee51146b03804b88eec9c85
revital-m <revital13m@gmail.com>
2022-01-10 09:49:58 +0200

    ex 2.1 - done

--- 2_1_file_system/2_1_file_system.js
+++ 2_1_file_system/2_1_file_system.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
+
+//? 1. Create a new txt file using the same fs module method we have learned before.
 // fs.writeFileSync('newTextFile.txt', 'Q-1: My new text file.');
 
+//? 2. Create a copy of the newly created txt file using a method from the fs module.
 // fs.copyFileSync('./newTextFile.txt', 'myCopyFile.txt');
 
+//? 3. Rename one of the files using a method from the fs module.
 // fs.renameSync('newTextFile.txt', 'changeName.txt');
 
+//? 4. Get a list of all the file names of the current directory using a method from the fs module.
+const myFolder = '../2_1_file_system/';
+fs.readdirSync(myFolder).forEach((file) => {
+  console.log(file);
+});
+
+//? 5. Find out and implement another method for the fs module.
+// fs.appendFileSync('myCopyFile.txt', '\nadding data');
