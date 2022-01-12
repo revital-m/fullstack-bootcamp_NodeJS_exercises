412b135bbf281a45b06fbe2c29b9586be41db1d0
revital-m <revital13m@gmail.com>
2022-01-12 13:46:24 +0200

    ex 9.1 - done

---
+++ 9_1_framworks/9_1_framworks.js
@@ -0,0 +1,17 @@
+/** 
+//? Socket.io- Chat Apps Made Easy with Socket.io:
+
+//* about:
+Socket.io is a Javascript library used in building real-time apps and establishing bidirectional communication between web clients and servers. With this library framework, you can develop applications with requirements for websocket development. For example, chat applications like Whatsapp continuously run for live updates and refresh the background process for updates or messages. It also offers real-time analytics in fewer lines of code.
+
+//* Features:
+1. Binary support (with a client-side library and a server-side library)
+2. Multiplexing support
+3. Reliability
+4. Auto-reconnection support
+5. Error detection & auto-correction
+6. Similar APIs for a client and server-side development When to use Socket.io
+7. Real-time
+8. Event-based communication 
+
+*/
